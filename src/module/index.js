842593bddd584fabf5441377327353ad1a909c42
Kene <kayyungx@gmail.com>
2022-10-11 12:02:30 +0100

    Add style for even list items

--- src/module/index.js
+++ src/module/index.js
@@ -1,1 +1,9 @@
 import '../styles/style.css';
+
+const list = document.querySelectorAll('.score-list');
+
+list.forEach((el, i) => {
+  if (i % 2 === 0) {
+    el.classList.add('bg-gray');
+  }
+});
